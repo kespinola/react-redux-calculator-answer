975d57168d25a44b18040ca6daa7611b99f22b59
Kyle Espinola <ksespinola@gmail.com>
2016-06-01 21:04:48 -0700

    create store that returns empty object for initial state

--- test/calculator-test.js
+++ test/calculator-test.js
@@ -1,6 +1,9 @@
 import { describe, it } from 'mocha';
-import { assert } from 'chai';
+import { expect } from 'chai';
+import store from './../src/store';
 
-describe('Calculator', () => {
-  it('can proccess tests', () => {});
+describe('Calculator State Management', () => {
+  it('sets initial state to empty object', () => {
+    expect(store.getState()).is.eql({});
+  });
 });
