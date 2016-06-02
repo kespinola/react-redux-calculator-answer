0b19714f5be7e328fe618e77502997d5670e9c75
Kyle Espinola <ksespinola@gmail.com>
2016-06-01 23:13:21 -0700

    support incrementing field

--- src/store.js
+++ src/store.js
@@ -1,7 +1,44 @@
 import { createStore } from 'redux';
+import { compose, ifElse } from 'ramda';
 
-const reducer = (state = {}, action) => {
-  switch (action.type) {
+// Actions Constants
+const INCREMENT_VALUE = 'calculator/INCREMENT_VALUE';
+
+// Actions Creators
+export const incrementValue = change => ({
+  type: INCREMENT_VALUE,
+  payload: change,
+});
+
+// State Updaters
+const isValidUpdate = ({ prev, update }) => {
+  return update;
+};
+
+const changeField = ({ prev, update }) => {
+  if (!prev) {
+    return update;
+  }
+  return prev += update;
+};
+
+const updateField = ifElse(
+  isValidUpdate,
+  changeField,
+  () => ''
+);
+
+export const reducer = (state = {}, { type, payload }) => {
+  switch (type) {
+    case INCREMENT_VALUE:
+      const change = {
+        prev: state.value,
+        update: payload,
+      };
+      return {
+        ...state,
+        value: updateField(change),
+      }
     default:
       return state;
   }
--- test/calculator-test.js
+++ test/calculator-test.js
@@ -1,9 +1,31 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
-import store from './../src/store';
+import store, { reducer, incrementValue } from './../src/store';
 
 describe('Calculator State Management', () => {
   it('sets initial state to empty object', () => {
     expect(store.getState()).is.eql({});
+  });
+  describe('INCREMENT_FIELD', () => {
+    it('sets value to empty string on empty increment value', () => {
+      const nextState = reducer({}, incrementValue());
+      expect(nextState).to.eql({ value: '' });
+    });
+    it('increments to 3 when current is empty', () => {
+      const nextState = reducer({}, incrementValue('3'));
+      expect(nextState).to.eql({ value: '3' });
+    });
+    it('increments to 34 when current is 3', () => {
+      const nextState = reducer({ value: '3' }, incrementValue('4'));
+      expect(nextState).to.eql({ value: '34' });
+    });
+    it('increments to 34. when current is 34', () => {
+      const nextState = reducer({ value: '34' }, incrementValue('.'));
+      expect(nextState).to.eql({ value: '34.' });
+    });
+    it('increments to 34.1 when current is 34.', () => {
+      const nextState = reducer({ value: '34.' }, incrementValue('1'));
+      expect(nextState).to.eql({ value: '34.1' });
+    });
   });
 });
